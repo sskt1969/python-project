4b30fde842f37c5a45ecb38d08761ab7a8757a6b
sskt1969 <41769896+sskt1969@users.noreply.github.com>
2018-07-31 12:43:50 +0530

    Create pro1.py

---
+++ pro1.py
@@ -0,0 +1,5 @@
+s =(input("Enter a character: "))
+if(s=='A' or s=='a' or s=='E' or s=='e' or s=='I' or s=='i' or s=='O' or s=='o' or s=='U' or s=='u'):
+  print(s, "is a vowel")
+else:
+  print(s, "is a consonant")
